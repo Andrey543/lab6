bc80ebf86ee427cadc42e5942973347905549f37
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-06 11:38:26 +0300

    Solution A

--- A.py
+++ A.py
@@ -1,13 +1,14 @@
 input=open('input.txt','r')
 output=open('output.txt','w')
 n=int(input.readline().rstrip())
-a=input.readline().rstrip() 
 f=True
 i=0
+x=input.read(1)
+a = list(map(int,input.readline().split()))
 while f:
-    if a.find(a[i],i+1)!=-1:
+    if a[i+1:].count(a[i])>0:
         f=False
         print(a[i],file=output)
-    i+=2
+    i+=1
 
 
