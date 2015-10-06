2d32386e40eec47178f3ff11a876ef039ab54f43
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-06 12:02:54 +0300

    Solution A

--- A.py
+++ A.py
@@ -4,8 +4,9 @@ n=int(input.readline().rstrip())
 f=True
 i=0
 a = list(map(int,input.readline().split()))
+a.sort()
 while f:
-    if a[i+1:].count(a[i])>0:
+    if a[i]==a[i+1]:
         f=False
         print(a[i],file=output)
     i+=1
