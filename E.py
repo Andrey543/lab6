7fd8493dfb6b84a79a40f12e4a5c86982dbcff37
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-10 00:27:27 +0300

    Solution D

--- E.py
+++ E.py
@@ -10,5 +10,5 @@ for i in range(k):
     a=input.readline().split()
     d[int(a[0])-1]-=int(a[2])
     d[int(a[1])-1]+=int(a[2])
-    for i in range(n):
-        print(d[i],end=' ',file=output)
+for i in range(n):
+    print(d[i],end=' ',file=output)
