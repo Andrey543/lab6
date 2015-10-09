7f60b216b75ee5a4e7ff8492f67017ed0beb4c30
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-10 00:15:26 +0300

    Solution D

--- D.py
+++ D.py
@@ -9,5 +9,6 @@ a=input.readline().split()
 for i in range(k-1):
     a1=input.readline().split()
     for j in range(n):
-        a[j]=min(a1[j],a[j])
-print(' '.join(a), file=output)
+        a[j]=min(int(a1[j]),int(a[j]))
+for i in range(n):
+    print(a[i],end=' ',file=output)
